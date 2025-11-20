c1d828f7c67e6b43ee421a15b587cae31b1ca75e
chris <kessell.chris21@gmail.com>
2025-11-20 10:55:27 -0500

    Add header file

--- BankersAlgorithm.h
+++ BankersAlgorithm.h
@@ -1,8 +1,24 @@
-//
-// Created by chris on 11/12/25.
-//
-
 #ifndef BANKERSALGORITHM_BANKERSALGORITHM_H
 #define BANKERSALGORITHM_BANKERSALGORITHM_H
+
+#include <vector>
+#include <string>
+
+class BankersAlgorithm {
+private:
+    int n; // number of processes
+    int m; // number of resource types
+    std::vector<int> total;
+    std::vector<int> available;
+    std::vector<std::vector<int>> allocation;
+    std::vector<std::vector<int>> maximum;
+    std::vector<std::vector<int>> need;
+
+public:
+    bool loadFromFile(const std::string& filename);
+    void calculateNeed();
+    void calculateAvailable();
+    bool isSafe(std::vector<int>& safeSequence);
+};
 
 #endif //BANKERSALGORITHM_BANKERSALGORITHM_H
