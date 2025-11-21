758abf40b64ab9928a6fa7596374208ee711fc6e
chris <kessell.chris21@gmail.com>
2025-11-21 14:00:33 -0500

    Implement main driver code for Banker's Algorithm

--- main.cpp
+++ main.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
+#include "BankersAlgorithm.h"
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+using namespace std;
+
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    BankersAlgorithm banker;
+    if (!banker.loadFromFile("input.txt"))
+        return 1;
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    banker.calculateNeed();
+    banker.calculateAvailable();
+
+    vector<int> safeSequence;
+    if (banker.isSafe(safeSequence)) {
+        cout << "System is in a safe state.\nSafe sequence: ";
+        for (size_t i = 0; i < safeSequence.size(); i++) {
+            cout << "P" << safeSequence[i];
+            if (i != safeSequence.size() - 1)
+                cout << " -> ";
+        }
+        cout << endl;
+    } else {
+        cout << "System is not in a safe state." << endl;
     }
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
